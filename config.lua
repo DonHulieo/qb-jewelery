872c4933c6d22ea11074ef788fac5c02675d887f
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2022-10-05 12:00:17 +1100

    Updated Config
    New store, cases, thermite and door checks in config.

--- config.lua
+++ config.lua
@@ -4,13 +4,49 @@ Config = Config or {}
 -- These have to be a string thanks to how Convars are returned.
 Config.UseTarget = GetConvar('UseTarget', 'false') == 'true'
 
-Config.Doorlock = "ox" -- qb or ox
-Config.DoorName = "doorID" -- edit this ID to your jewelery store door ID
+Config.Doorlock = "qb" -- qb or ox
+Config.CityDoor = "jewelery-citymain" -- edit this ID to your city jewelery store door ID
+Config.CitySec = "jewelery-citysec"
+Config.GrapeDoor = "jewelery-grapemain" -- edit this ID to your grapeseed jewelery store door ID
+Config.GrapeSec = "jewelery-grapesec"
+--Config.PalDoor = "jewelery-citymain" -- edit this ID to your paleto jewelery store door ID
 
-Config.Timeout = 30 * (60 * 2000)
-Config.RequiredCops = 2
+Config.ShowBlips = "true" -- blips for stores
+
+Config.Cooldown = 5 * (60 * 2000) -- where x is minutes ie. x * (60 * 2000) \\ For door auto lock function
+Config.Timeout = 5 * (60 * 2000) -- where x is minutes ie. x * (60 * 2000) \\ For actual robberry cooldown
+Config.RequiredCops = 0
+
+Config.DoorItem = 'thermite' -- item to remove\check for when placing a charge
+Config.ThermiteSettings = {
+    correctBlocks = 1, -- correctBlocks = Number of correct blocks the player needs to click
+    incorrectBlocks = 4, -- incorrectBlocks = number of incorrect blocks after which the game will fail
+    timetoShow = 4.5, -- timetoShow = time in secs for which the right blocks will be shown
+    timetoLose = 12 -- timetoLose = maximum time after timetoshow expires for player to select the right blocks
+}
+
 Config.JewelleryLocation = {
-    ["coords"] = vector3(-630.5, -237.13, 38.08),
+    ["cityvangelico"] = {
+        ["coords"] = vector3(-630.5, -237.13, 38.08)
+    },
+    ["grapevangelico"] = {
+        ["coords"] = vector3(1649.78, 4882.32, 42.16)
+    },
+    --[[["palvangelico"] = {
+        ["coords"] = vector3(-630.5, -237.13, 38.08)
+    },]]
+}
+
+Config.Jewelery = {
+    ["city"] = { -- City Vangelico's \\ Rooftop Thermite
+        {coords = vector4(-596.02, -283.7, 50.4, 304.5), anim = vector4(-596.02, -283.7, 50.4, 304.5), effect = vector3(-596.02, -283.7, 50.19), isOpen = false},
+    },
+    ["grape"] = { -- Grapeseed Vangelico's \\ Side Alley Thermite
+        {coords = vector4(1645.07, 4867.87, 42.03, 16.33), anim = vector4(1645.07, 4867.87, 42.03, 16.33), effect = vector(1645.07, 4867.87, 41.84), isOpen = false},
+    },
+    --[[["pal"] = { -- Paleto Vangelico's \\ Rear of Building Thermite
+        {coords = vector4(-596.02, -283.7, 50.4, 304.5), anim = vector4(-596.02, -283.7, 50.4, 304.5), effect = vector3(-596.02, -283.7, 50.19), isOpen = false},
+    },]]
 }
 
 Config.WhitelistedWeapons = {
@@ -176,6 +212,46 @@ Config.Locations = {
         ["coords"] = vector3(-623.94, -228.05, 38.05),
         ["isOpened"] = false,
         ["isBusy"] = false,
+    },
+    [21] = {
+        ["coords"] = vector3(1650.06, 4885.96, 41.66),
+        ["isOpened"] = false,
+        ["isBusy"] = false,
+    },
+    [22] = {
+        ["coords"] = vector3(1648.81, 4885.74, 41.66),
+        ["isOpened"] = false,
+        ["isBusy"] = false,
+    },
+    [23] = {
+        ["coords"] = vector3(1646.31, 4884.01, 41.66),
+        ["isOpened"] = false,
+        ["isBusy"] = false,
+    },
+    [24] = {
+        ["coords"] = vector3(1647.37, 4882.94, 41.66),
+        ["isOpened"] = false,
+        ["isBusy"] = false,
+    },
+    [25] = {
+        ["coords"] = vector3(1648.68, 4883.08, 41.66),
+        ["isOpened"] = false,
+        ["isBusy"] = false,
+    },
+    [26] = {
+        ["coords"] = vector3(1648.98, 4881.3, 41.66),
+        ["isOpened"] = false,
+        ["isBusy"] = false,
+    },
+    [27] = {
+        ["coords"] = vector3(1647.66, 4881.12, 41.66),
+        ["isOpened"] = false,
+        ["isBusy"] = false,
+    },
+    [28] = {
+        ["coords"] = vector3(1646.92, 4879.76, 41.66),
+        ["isOpened"] = false,
+        ["isBusy"] = false,
     }
 }
 
@@ -185,10 +261,4 @@ Config.MaleNoHandshoes = {
 
 Config.FemaleNoHandshoes = {
     [0] = true, [1] = true, [2] = true, [3] = true, [4] = true, [5] = true, [6] = true, [7] = true, [8] = true, [9] = true, [10] = true, [11] = true, [12] = true, [13] = true, [14] = true, [15] = true, [19] = true, [59] = true, [60] = true, [61] = true, [62] = true, [63] = true, [64] = true, [65] = true, [66] = true, [67] = true, [68] = true, [69] = true, [70] = true, [71] = true, [129] = true, [130] = true, [131] = true, [135] = true, [142] = true, [149] = true, [153] = true, [157] = true, [161] = true, [165] = true,
-}
-
-Config.Jewelery = {
-    ["thermite"] = {
-        {coords = vector4(-596.02, -283.7, 50.4, 304.5), anim = vector4(-596.02, -283.7, 50.4, 304.5), effect = vector3(-596.02, -283.7, 50.19), isOpen = false},
-    },
 }
